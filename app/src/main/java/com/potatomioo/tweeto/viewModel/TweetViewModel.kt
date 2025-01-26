25385d9bc9a81869065a0113b6ff8e8e356b2899
Deeptanshu Shukla <deeptanshushuklaji@gmail.com>
2025-01-26 13:10:34 +0530

    Called category from savedStateHandle

--- app/src/main/java/com/potatomioo/tweeto/viewModel/TweetViewModel.kt
+++ app/src/main/java/com/potatomioo/tweeto/viewModel/TweetViewModel.kt
@@ -1,5 +1,7 @@
 package com.potatomioo.tweeto.viewModel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.SavedStateViewModelFactory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.potatomioo.tweeto.models.tweetsItem
@@ -10,14 +12,18 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class TweetViewModel @Inject constructor(private val repository: repository) : ViewModel(){
+class TweetViewModel @Inject constructor(
+    private val repository: repository,
+    val savedStateHandle: SavedStateHandle
+) : ViewModel(){
 
     val _tweets : StateFlow<List<tweetsItem>>
         get() = repository.tweets
 
     init {
         viewModelScope.launch {
-            repository.getTweets("sport")
+            val category = savedStateHandle.get<String>("category") ?: "sport"
+            repository.getTweets(category)
         }
     }
 }
